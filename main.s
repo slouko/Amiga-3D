b0b3b1bb610b06bf4f200ecac85e73942bfe3056
Ignacio Soriano Hernandez <ignacio.soriano@me.com>
2025-06-25 22:59:21 +0200

    Build script changed and main.s set to default torus

--- main.s
+++ main.s
@@ -18,16 +18,27 @@ clip_bottom = screen_height-1
 
 ; object creation parameters and modifiers
 ; calculated basic shape is a torus. 
-obj_circles = 32 ; torus circles (8,16,32 or 64)
-obj_rounds = 16 ; torus rounds (8,16,32 or 64)
-obj_width = 15000 ; torus width
-obj_thick = 500 ; shape thickness
-obj_scale = 2500 ; shape scale
-obj_step = 0 ; shape modifier
-obj_factor = 16 ; shape modifier depth
+; obj_circles = 32 ; torus circles (8,16,32 or 64)
+; obj_rounds = 16 ; torus rounds (8,16,32 or 64)
+; obj_width = 15000 ; torus width
+; obj_thick = 500 ; shape thickness
+; obj_scale = 2500 ; shape scale
+; obj_step = 0 ; shape modifier
+; obj_factor = 16 ; shape modifier depth
+; vertices = obj_circles*obj_rounds
+; surfaces = vertices*2
+; zoomlevel = 3000
+
+obj_circles = 16 ; torus circles (8,16,32 or 64)
+obj_rounds = 32 ; torus rounds (8,16,32 or 64)
+obj_width = 20000 ; torus width
+obj_thick = 600 ; shape thickness
+obj_scale = 4000 ; shape scale
+obj_step = 128 ; shape modifier
+obj_factor = 32 ; shape modifier depth
 vertices = obj_circles*obj_rounds
 surfaces = vertices*2
-zoomlevel = 3000
+zoomlevel = 900
 
                 ; all routines other than generic subroutines are splitted into multiple
                 ; files for easier access and better readability
